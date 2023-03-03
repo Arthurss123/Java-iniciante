8ef000a03f14bf3689023f3fbf4f17ade0fb9f8f
Arthur Moura <120514370+Arthurss123@users.noreply.github.com>
2023-03-03 17:39:53 -0300

    Projeto Smart Tv

--- primeiro-java/src/Usuario.java
+++ primeiro-java/src/Usuario.java
@@ -9,6 +9,14 @@ public class Usuario{
         System.out.println("A tv esta em qual volume?" + smartTv.volume);
 
         
+        smartTv.ligar();
+        System.out.println("Novo status?" + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        System.out.println("Novo volume?" + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        
     }
 
 }
