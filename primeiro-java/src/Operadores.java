96396187dd02408022528b4d5a0933b928ea0cd2
Arthur Moura <120514370+Arthurss123@users.noreply.github.com>
2023-03-03 16:05:28 -0300

    Update Operadores.java

--- primeiro-java/src/Operadores.java
+++ primeiro-java/src/Operadores.java
@@ -1,7 +1,11 @@
 public class Operadores {
      public static void main(String[] args) {
 
-        
+        int numero = 5;
+        //repetição
+        numero++;
+        //Mesma coisa de numero = numero + 1;
+        System.out.println(numero);
     }
 
 
